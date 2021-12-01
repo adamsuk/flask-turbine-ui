8303526159f698cdc729db426b47f4adee578be7
Scott <sra405@hotmail.co.uk>
2021-12-01 20:51:35 +0000

    general tidy up

--- src/app.py
+++ src/app.py
@@ -1,23 +1,29 @@
-import json
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 from data_request import TurbineData
+
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/turbine/<name>')
 def html_table(name=None):
-    tb = TurbineData()
+    tb = TurbineData(url="https://run.mocky.io/v3/6a13fe7e-840c-4d82-b58f-c737139f0e55")
     if tb.error:
-        return  render_template("turbine_list.html", error=tb.error)
+        return  render_template("turbine_list.html",
+                                error=tb.error)
     else:
         if name:
             df = tb.data.loc[tb.data['turbine'] == name]
         else:    
             df = tb.formatted_data
         # link_column is the column that I want to add a button to
-        return render_template("turbine_list.html", column_names=list(df.columns.values), row_data=list(df.values.tolist()),
-                                link_column="Name", zip=zip, name=name, error=tb.error)
+        return render_template("turbine_list.html",
+                               column_names=list(df.columns.values),
+                               row_data=list(df.values.tolist()),
+                               link_column="Name",
+                               zip=zip,
+                               name=name,
+                               error=tb.error)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- src/data_request.py
+++ src/data_request.py
@@ -3,7 +3,7 @@ import requests
 
 
 class TurbineData:
-    def __init__(self, url="https://run.mocky.io/v3/6a13fe7e-840c-4d82-b58f-c737139f0e55"):
+    def __init__(self, url=None):
         self.url = url
         self.data = pd.DataFrame()
         self.formatted_data = None
@@ -17,22 +17,20 @@ class TurbineData:
     def fetch_data(self):
         try:
             r = requests.get(self.url)
-            r.raise_for_status()
             self.data = pd.DataFrame(r.json())
         except requests.exceptions.HTTPError as err:
             self.error = err
 
     def format_data(self):
         self.formatted_data = pd.DataFrame()
-        for index, row in self.data.iterrows():
-            print('LEVEL {}: {}'.format(row['level'], row))
+        for _, row in self.data.iterrows():
             self.formatted_data.loc[row['turbine'], 'Name'] = row['turbine']
             if row['level'] == 1:
                 level = 'WARNING'
             elif row['level'] == 2:
                 level = 'CRITICAL'
             self.formatted_data.loc[row['turbine'], level] = row['count']
+        # fill in NaNs and sort primary - CRITICAL, secondary - WARNING
         self.formatted_data = self.formatted_data.fillna(0)
         self.formatted_data = self.formatted_data.sort_values(['WARNING'], ascending=[False])
         self.formatted_data = self.formatted_data.sort_values(['CRITICAL'], ascending=[False])
-        print(self.formatted_data)
